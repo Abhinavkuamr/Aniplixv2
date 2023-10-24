b635a48a66414b786aeea642cf18a1f62e8bf968
Abhinav <ady.abhinav.ak@gmail.com>
2023-10-24 17:41:08 +0100

    Update Row.js

--- src/Component/Row.js
+++ src/Component/Row.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Stylesheet/row.css';
-import axios from '../axios';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 function Row({ title, fetchUrl }) {
