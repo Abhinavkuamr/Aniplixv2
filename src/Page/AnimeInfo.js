6c30e391cbb2926966545d8dee519036683d3b6d
Abhinav <ady.abhinav.ak@gmail.com>
2023-10-24 17:40:19 +0100

    Update AnimeInfo.js

--- src/Page/AnimeInfo.js
+++ src/Page/AnimeInfo.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './stylesheet/AnimeInfo.css';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
-import axios from '../axios';
+import axios from 'axios';
 
 function AnimeInfo() {
   const [anime, setAnime] = useState({});
